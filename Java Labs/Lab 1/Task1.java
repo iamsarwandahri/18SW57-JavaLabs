0bf3c51ccec0cd8913c6ae6065bec2955fae54dd
Sarwan Dahri <49364347+iamsarwandahri@users.noreply.github.com>
2019-04-08 01:41:26 +0500

    Sarwan Dahri - 18SW57 (SEC 2)

---
+++ Java Labs/Lab 1/Task1.java
@@ -0,0 +1,22 @@
+class Task1{
+	public static void main(String args[]){
+		String name;
+		int age;
+		float per;
+		boolean pass;
+		char grade;
+   
+		name = "Sarwan Dahri";
+		age = 18;
+		per = 75;
+        pass  = true;
+        grade = 'A';
+   
+        System.out.println(" Name: "+name+"\n Age: "+age+"\n Percentage: "+per+"\n Status: "+pass+"\n Grade "+grade);
+        
+
+
+
+
+	}//end of main
+}//end of class
