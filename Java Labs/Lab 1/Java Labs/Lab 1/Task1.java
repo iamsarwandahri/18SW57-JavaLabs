9b48153097f42165b5eb4de58bb8596efa411c3c
Sarwan Dahri <49364347+iamsarwandahri@users.noreply.github.com>
2019-04-08 02:31:11 +0500

    Java Programs
    Sarwan Dahri
    18SW57

---
+++ Java Labs/Lab 1/Java Labs/Lab 1/Task1.java
@@ -0,0 +1,27 @@
+class Task1{
+	public static void main(String args[]){
+		String name;
+		byte age;
+		short marks;
+		float per;
+		String status;
+		char grade;
+        double percl;
+        int year;
+
+		name = "Sarwan Dahri";
+		age = 18;
+		marks = 550;
+		per = 75.67f;
+        status = "Pass";
+        grade = 'A';
+        percl = 94.5;
+        year = 2018;
+        System.out.println(" Name: "+name+"\n Age: "+age+"\n Marks: "+marks+"\n Percentage: "+per+"\n Status: "+status+"\n Grade: "+grade+"\n Percentage of Class Attended: "+percl+"\n Year: "+year);
+        
+
+
+
+
+	}//end of main
+}//end of class
---
+++ Java Labs/Lab 1/Java Labs/Lab 1/Task2.java
@@ -0,0 +1,11 @@
+class Task2{
+	public static void main(String args[]){
+	double dub = 50.5656;
+	long lg = (int)dub;
+    double db = dub - lg;
+    System.out.println("Number: " + dub + "\nWhole part: " + lg + " \nDecimal part: " + db ); 
+}//end of main
+
+
+
+}//end of class
